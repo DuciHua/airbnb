0db60b5b0533b1cab7d11ba95a0d0befc2c1ad2e
Trung Duc Hua <ducihua@Trungs-MacBook-Pro.local>
2025-07-04 00:35:25 +0700

    Description goes here

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     container: {
       center: true,
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'ui-sans-serif', 'system-ui'],
+      },
+    },
   },
   plugins: [require('flowbite/plugin')],
 }
